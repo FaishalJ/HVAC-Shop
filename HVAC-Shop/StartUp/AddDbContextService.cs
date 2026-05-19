97a840dac12bbc045970f614995316eec1dfb67b
faishalJ <faishaljaffar@gmail.com>
2026-05-19 14:57:44 +0300

    Enable transient fault resiliency

--- HVAC-Shop/StartUp/AddDbContextService.cs
+++ HVAC-Shop/StartUp/AddDbContextService.cs
@@ -9,7 +9,11 @@ namespace HVAC_Shop.StartUp
         public static void AddDatabaseServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                sqlOptions => sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+					maxRetryDelay: TimeSpan.FromSeconds(30),
+					errorNumbersToAdd: null))
                 .UseAsyncSeeding(async (context, _, cancellationToken) =>
                 {
                     var dbContext = (AppDbContext)context;
